475bc169c8fa00bfff4864dbce3198985a65de74
Nick McClure <nickjm@uky.edu>
2023-03-21 11:13:43 -0400

    Add project files.

---
+++ GraphSDK5GroupBug/Program.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+
+Console.WriteLine("Get Groups");
+
+string graphURL = "";
+string tenantId = "";
+string clientId = "";
+string secret = "";
+string upn = "";
+
+string[] scopes = new[] { $"{graphURL}/.default" };
+ClientSecretCredential clientCredential = new(tenantId, clientId, secret);
+GraphServiceClient graphServiceClient = new(clientCredential, scopes);
+
+var groupResponse = await graphServiceClient
+    .Users[upn]
+    .MemberOf
+    .GetAsync(requestConfiguration =>
+    {
+        requestConfiguration.QueryParameters.Select = new string[] { "DisplayName" };
+    });
+
+
+var pageIterator = PageIterator<Group, GroupCollectionResponse>
+    .CreatePageIterator(graphServiceClient, groupResponse!,
+        (g) =>
+        {
+            Console.WriteLine(g.DisplayName);
+            return true;
+        }
+    );
+
+await pageIterator.IterateAsync();
+
+
+
